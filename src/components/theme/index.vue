a8366e736e3a51ac43ee041f8d5cf57a4fdcda60
lzw <378406712@qq.com>
2020-05-14 01:31:53 +0800

    style: theme

--- src/components/theme/index.vue
+++ src/components/theme/index.vue
@@ -10,40 +10,36 @@
       <el-dropdown-item class="clearfix" command="science-blue">
         深空蓝
       </el-dropdown-item>
-       <el-dropdown-item class="clearfix" command="sundown-red">
-        日落红
+      <el-dropdown-item class="clearfix" command="sundown-red">
+        樱花粉
       </el-dropdown-item>
-        <el-dropdown-item>
-          <router-link to="/theme/index">
-        更多皮肤</router-link>
+      <el-dropdown-item>
+        <router-link to="/theme/index"> 更多皮肤</router-link>
       </el-dropdown-item>
-      
-      
     </el-dropdown-menu>
-    
   </el-dropdown>
 </template>
 
 <script>
 import '@/assets/custom-theme/sundown-red.css' // the theme changed version element-ui css
-import '@/assets/custom-theme/science-blue.css' 
+import '@/assets/custom-theme/science-blue.css'
 import { addClass, removeClass } from '@/utils'
 export default {
   methods: {
     // change theme - 换肤
     changeTheme(command) {
-      if(command === 'default') {
+      if (command === 'default') {
         removeClass(document.body)
-      
+
         return
       }
-      if(command === 'science-blue') {
+      if (command === 'science-blue') {
         removeClass(document.body)
         addClass(document.body, 'science-blue')
         return
       }
-     if(command === 'sundown-red') {
-       removeClass(document.body)
+      if (command === 'sundown-red') {
+        removeClass(document.body)
         addClass(document.body, 'sundown-red')
         return
       }
