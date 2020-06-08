ca4ac02a0cf2405e653d54ffa9679d8c2898b6b4
lzw <378406712@qq.com>
2020-06-09 00:49:40 +0800

    style: echarts

--- src/views/echarts/components/LineChart.vue
+++ src/views/echarts/components/LineChart.vue
@@ -1,14 +1,109 @@
 <template>
-  <div class="echartsPanel"></div>
+  <div :class="className" :style="{ height: height, width: width }" />
 </template>
 
 <script>
 import echarts from 'echarts'
-require('echarts/theme/macarons') // echarts theme
+require('echarts/theme/sakura') // echarts theme
 
 export default {
-
-  
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '350px'
+    },
+    autoResize: {
+      type: Boolean,
+      default: true
+    },
+    chartData: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    initCharts() {
+      this.chart = echarts.init(this.$el, 'sakura')
+      this.setOptions(this.chartData)
+    },
+    setOptions({ Pixiv, Message, Flower, Shopping } = {}) {
+      this.chart.setOption({
+        title: {
+          text: '访问量'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          },
+          padding: [5, 10]
+        },
+        legend: {
+          data: ['Pixiv', 'Message', 'Flower', 'Shopping']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: 'Pixiv',
+            type: 'line',
+            stack: '总量',
+            data: Pixiv,
+            smooth: true
+          },
+          {
+            name: 'Message',
+            type: 'line',
+            stack: '总量',
+            data: Message,
+            smooth: true
+          },
+          {
+            name: 'Flower',
+            type: 'line',
+            stack: '总量',
+            data: Flower,
+            smooth: true
+          },
+          {
+            name: 'Shopping',
+            type: 'line',
+            stack: '总量',
+            data: Shopping,
+            smooth: true
+          }
+        ]
+      })
+    }
+  },
+  mounted() {
+    this.initCharts()
+  }
 }
 </script>
 
--- src/views/echarts/index.vue
+++ src/views/echarts/index.vue
@@ -5,7 +5,7 @@
     <el-row
       style="background: #fff; padding: 16px 16px 0; margin-bottom: 32px;"
     >
-      <line-chart />
+      <line-chart :chart-data="lineChartData" />
     </el-row>
   </div>
 </template>
@@ -13,10 +13,23 @@
 <script>
 import PanelGroup from './components/PanelGroup'
 import LineChart from './components/LineChart'
+const lineChartData = {
+  Pixiv: [100, 120, 161, 134, 105, 160, 165],
+  Message: [200, 192, 120, 144, 160, 130, 140],
+
+  Flower: [80, 100, 121, 104, 105, 90, 100],
+
+  Shopping: [130, 140, 141, 142, 145, 150, 160]
+}
 export default {
   components: {
     PanelGroup,
     LineChart
+  },
+  data() {
+    return {
+      lineChartData
+    }
   }
 }
 </script>
